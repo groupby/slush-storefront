09012ad6cd0be786a25e8998b580dfd10b2af58f
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-01-29 10:32:55 -0500

    Remove example component files

--- templates/advanced/src/tags/components/example/example.js
+++
@@ -1,8 +0,0 @@
-import { tag } from '@storefront/core';
-
-@tag('example-component', require('./example.html'), require('./example.css'))
-export class ExampleComponent {
-
-}
-
-export default ExampleComponent;
