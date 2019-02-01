375be235f05f9fc67e91b48a589138571c8666c4
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-02-01 08:31:55 -0500

    Remove console logs from lifecycle methods

--- templates/component/src/index.js
+++ templates/component/src/index.js
@@ -3,27 +3,27 @@ import { tag } from '@storefront/core';
 @tag('<%= slug %>', require('./<%= slug %>.html'), require('./<%= slug %>.scss'))
 export class <%= sanitizedName %> {
   onBeforeMount() {
-    console.log('__ ...'); // TEMP
+
   }
 
   onMount() {
-    console.log('__ ...'); // TEMP
+
   }
 
   onUpdate() {
-    console.log('__ ...'); // TEMP
+
   }
 
   onUpdated() {
-    console.log('__ ...'); // TEMP
+
   }
 
   onBeforeUnmount() {
-    console.log('__ ...'); // TEMP
+
   }
 
   onUnmount() {
-    console.log('__ ...'); // TEMP
+
   }
 }
 
